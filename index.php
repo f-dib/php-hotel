1264cbe1d6e0655a0383ab3fc7c91bc2feff6b96
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-19 03:13:43 +0100

    exercise completed

--- index.php
+++ index.php
@@ -41,8 +41,8 @@
     ];
 
     $hotelKeys = array_keys($hotels[0]);
-
     $parking = $_GET['parking'];
+    $vote = $_GET['vote'];
 
     if($parking) {
 
@@ -50,6 +50,12 @@
             return $hotels['parking'];
         });
 
+    };
+
+    if ($vote) {
+        $hotels = array_filter($hotels, function($hotel)use($vote){
+          return $hotel['vote'] == $vote;
+        });
     };
 
 ?>
@@ -61,40 +67,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
     <div class="container-xxl py-5">
-        <form>
-            <div class="mb-3">
-                <label for="parking" class="form-check-label">Mostra solo con parcheggio</label>
+        <div class="p-5 w-25 m-auto"><img class="img-fluid" src="./img/1280px-Booking.com_logo.svg.png" alt=""></div>
+        <form class="d-flex gap-5 align-items-center justify-content-around mb-5">
+            <div class="w-75">
+                <select class="form-select" aria-label="Hotel Vote" name="vote">
+                    <option value="">Qualsiasi Valutazione</option>
+                    <option value="1">1 Stella</option>
+                    <option value="2">2 Stelle</option>
+                    <option value="3">3 Stelle</option>
+                    <option value="4">4 Stelle</option>
+                    <option value="5">5 Stelle</option>
+                </select>
+            </div>
+            <div class="text-nowrap">
+                <label for="parking" class="form-check-label">Mostra con parcheggio</label>
                 <input type="checkbox" class="form-check-input" id="parking" name="parking">
             </div>
-            <input type="submit">
+            <input type="submit" class="rounded-4 px-2 py-1 bg-primary text-white border-white">
         </form>
-        <table class="table">
-            <thead>
-                <tr>
+        <div class="table-responsive card">
+            <table class="table table-bordered table-striped m-0">
+                <thead>
+                    <tr class="table-primary">
+                        <?php
+                            foreach ($hotelKeys as $key){
+                                echo "<th>$key</th>";    
+                            }
+                        ?>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                        foreach ($hotelKeys as $key){
-                            echo "<th>$key</th>";    
+                        foreach ($hotels as $currentHotel){
+                            echo "<tr>";
+                                foreach ($currentHotel as $currentInfo){
+                                    echo "<td> $currentInfo </td>";
+                                }
+                            echo "</tr>";
                         }
                     ?>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach ($hotels as $currentHotel){
-                        echo "<tr>";
-                            foreach ($currentHotel as $currentInfo){
-                                echo "<td> $currentInfo </td>";
-                            }
-                        echo "</tr>";
-                    }
-                ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 
